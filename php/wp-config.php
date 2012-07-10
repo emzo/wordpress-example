b2e7f7ae4de5fbe2188590cc4c9e01404efc46c3
Emyr Thomas <emyr.thomas@gmail.com>
2012-07-10 12:13:50 +0100

    Keep plugins, themes and uploads in OPENSHIFT_DATA_DIR

--- php/wp-config.php
+++ php/wp-config.php
@@ -1,50 +1,35 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
+	define( 'WP_LOCAL_DEV', true );
+	include( dirname( __FILE__ ) . '/local-config.php' );
+} else {
+	define( 'WP_LOCAL_DEV', false );
+	define( 'DB_NAME', $_ENV['OPENSHIFT_APP_NAME'] );
+	define( 'DB_USER', $_ENV['OPENSHIFT_DB_USERNAME'] );
+	define( 'DB_PASSWORD', $_ENV['OPENSHIFT_DB_PASSWORD'] );
+	define( 'DB_HOST', $_ENV['OPENSHIFT_DB_HOST'] );
+}
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', $_ENV['OPENSHIFT_DATA_DIR'] );
 
-/** MySQL database username */
-define('DB_USER', $_ENV['OPENSHIFT_DB_USERNAME']);
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
 
-/** MySQL database password */
-define('DB_PASSWORD', $_ENV['OPENSHIFT_DB_PASSWORD']);
-
-/** MySQL hostname */
-define('DB_HOST', $_ENV['OPENSHIFT_DB_HOST']);
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
-
+// ========================================================================
+// Salts, for security
 // This is where we define the OpenShift specific secure variable functions
-require_once(ABSPATH . '/wp-includes/openshift.inc');
+// ========================================================================
+
+require_once('openshift.php');
 
 // Set the default keys to use
 $_default_keys = array(
@@ -64,7 +49,7 @@ function make_secure_key($args) {
   $key  = $args['variable'];
   $original = $args['original'];
 
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   $chars .= '!@#$%^&*()';
   $chars .= '-_ []{}<>~`+=,.;:/?|';
 
@@ -89,40 +74,46 @@ foreach ($array as $key => $value) {
   define($key,$value);
 }
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+// ==============================================================
+// Table prefix
+// Change this if you have multiple installs in the same database
+// ==============================================================
 $table_prefix  = 'wp_';
 
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
+// ================================
+// Language
+// Leave blank for American English
+// ================================
+define( 'WPLANG', '' );
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
+// ===========
+// Hide errors
+// ===========
+//ini_set( 'display_errors', 0 );
+//define( 'WP_DEBUG_DISPLAY', false );
 
-/* That's all, stop editing! Happy blogging. */
+// =================================================================
+// Debug mode
+// Debugging? Enable these. Can also enable them in local-config.php
+// =================================================================
+// define( 'SAVEQUERIES', true );
+define( 'WP_DEBUG', true );
 
-/** Absolute path to the WordPress directory. */
-if ( !defined('ABSPATH') )
-  define('ABSPATH', dirname(__FILE__) . '/');
+// ======================================
+// Load a Memcached config if we have one
+// ======================================
+if ( file_exists( dirname( __FILE__ ) . '/memcached.php' ) )
+	$memcached_servers = include( dirname( __FILE__ ) . '/memcached.php' );
 
-/** Sets up WordPress vars and included files. */
-require_once(ABSPATH . 'wp-settings.php');
+// ===========================================================================================
+// This can be used to programatically set the stage when deploying (e.g. production, staging)
+// ===========================================================================================
+define( 'WP_STAGE', '%%WP_STAGE%%' );
+define( 'STAGING_DOMAIN', '%%WP_STAGING_DOMAIN%%' ); // Does magic in WP Stack to handle staging domain rewriting
+
+// ===================
+// Bootstrap WordPress
+// ===================
+if ( !defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname( __FILE__ ) . '/wp/' );
+require_once( ABSPATH . 'wp-settings.php' );
